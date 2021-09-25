ecea8f495791a3e4b00931f801095f225084c5da
RaameshB <geoplexofficial@gmail.com>
2021-09-24 20:08:28 -0500

    a commit ig

---
+++ RaameshCode/src/main/java/org/firstinspires/ftc/teamcode/UtilityLibs/HelperClasses/PhantomOpMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.UtilityLibs.HelperClasses;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.UtilityLibs.MecanumLibs.MecanumHelper;
+
+public abstract class PhantomOpMode extends LinearOpMode {
+
+    public LinearOpMode haha = this;
+    public TelemetryHelper telemetry = new TelemetryHelper(this);
+    public RobotConfig robot;
+    public MecanumHelper chassis;
+    public Stopwatch stopwatch = new Stopwatch(this);
+
+    public void setChassis(chassisType type){
+        robot =  new RobotConfig(hardwareMap, this, telemetry);
+        robot.robotType = type;
+        if (type == chassisType.MECANUM) {
+             chassis = new MecanumHelper(robot, telemetry, this);
+        }
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
