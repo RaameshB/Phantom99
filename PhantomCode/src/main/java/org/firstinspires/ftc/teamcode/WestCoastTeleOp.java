89b380ccdeb89b86229b04507b63f6ba84825e59
TheLastWord67 <90577831+TheLastWord67@users.noreply.github.com>
2021-10-08 13:49:35 -0500

    WestCoastTeleOp v1

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastTeleOp.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/WestCoastTeleOp.java
@@ -13,14 +13,37 @@ public class WestCoastTeleOp extends LinearOpMode {
     RobotConfig robot = new RobotConfig();
     double leftDrivePower;
     double rightDrivePower;
-    float powerMult;
+    float powerMult = 5;
+    boolean isBumperPressed = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap, this);
 
+        while(!isStopRequested()){
 
-        robot.leftDrive.setPower(leftDrivePower);
-        robot.rightDrive.setPower(rightDrivePower);
+            leftDrivePower = gamepad1.left_stick_y;
+            rightDrivePower = gamepad1.right_stick_y;
+
+            if(!gamepad1.left_bumper && !isBumperPressed && powerMult > 1) {
+                powerMult -= 1;
+                isBumperPressed = true;
+
+            }
+
+
+            if(!gamepad1.right_bumper && !isBumperPressed && powerMult < 10){
+                powerMult += 1;
+                isBumperPressed = true;
+            }
+
+
+            robot.leftDrive.setPower(leftDrivePower);
+            robot.rightDrive.setPower(rightDrivePower);
+
+            idle();
+        }
+
+
     }
 }
