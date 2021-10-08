47df173edfc7cff97affe8f54dca15d2eca71363
TheLastWord67 <90577831+TheLastWord67@users.noreply.github.com>
2021-10-08 12:37:14 -0500

    -_- v2

---
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/UtilityLibs/MotorCluster.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.UtilityLibs;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+public class MotorCluster {
+
+    HardwareMap hardwareMap;
+    DcMotor motor1;
+    DcMotor motor2;
+
+    public void setMotors(String motor1Name, String motor2Name, HardwareMap aHwMap) {
+        hardwareMap = aHwMap;
+        hardwareMap.get(DcMotor.class, motor1Name);
+        hardwareMap.get(DcMotor.class, motor2Name);
+    }
+
+    public void setDirection(DcMotorSimple.Direction direction) {
+        motor1.setDirection(direction);
+        motor2.setDirection(direction);
+    }
+
+    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+
+        motor1.setZeroPowerBehavior(zeroPowerBehavior);
+        motor2.setZeroPowerBehavior(zeroPowerBehavior);
+    }
+
+    public void setMode(DcMotor.RunMode runMode){
+        motor1.setMode(runMode);
+        motor2.setMode(runMode);
+    }
+
+
+}
